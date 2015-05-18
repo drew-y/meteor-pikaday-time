1c820c91fbca24f344103acd0e07a2807fc75328
Andrew Youngwerth <drew.youngwerth@gmail.com>
2015-05-18 00:54:14 -0600

    Initial commit

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'drewy:pikaday-time',
+  version: '1.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'Simple DateTime Picker',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/drew-y/meteor-pikaday-time',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles(['pikaday.js', 'pikaday.css'], 'client');
+});
